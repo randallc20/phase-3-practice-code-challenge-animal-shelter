121651e13fa4054a6b8507bedcf684196e613a11
brewchetta <chett.tiller@gmail.com>
2022-04-19 09:12:44 -0400

    solution added

--- app/models/adopter.rb
+++ app/models/adopter.rb
@@ -1,3 +1,24 @@
 class Adopter < ActiveRecord::Base
+  has_many :pets, dependent: :destroy
+  has_many :shelters, through: :pets
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def adopt(pet)
+    if !pet.adopted? && !pet.adopter_id
+      pet.update(adopted?: true, adopter_id: self.id)
+    end
+  end
+
+  def fav_pet
+    return nil if self.pets.count == 0
+
+    self.pets
+    .group_by { |pet| pet.species}
+    .max_by { |species| species.count }
+    [0]
+  end
 
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,13 @@
 class Shelter < ActiveRecord::Base
+  has_many :pets, dependent: :destroy
+  has_many :adopters, through: :pets
+
+  def current_pets
+    self.pets.where(adopted?: false)
+  end
+
+  def adopted_pets
+    self.pets.where(adopted?: true)
+  end
 
 end
